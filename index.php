a0861858fbef488fc163b036d08c11f46ad33da4
unapurnama <unnaningsi@gmail.com>
2024-06-22 03:01:45 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require 'koneksi.php';
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query_sql = "SELECT * FROM tbl_user WHERE email = '$email' AND password = '$password'";
+
+$result = mysqli_query($conn, $query_sql);
+
+if (mysqli_num_rows($result) > 0) {
+    header("Location: koneksi.php");
+} else {
+    echo "<center><h1>Email atau Password Anda Salah. Silahkan Coba Login Kembali.</h1>
+            <button><strong><a href='index.html'>Login</a></strong></button></center>";
+}
